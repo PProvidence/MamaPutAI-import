6862eb3e4bbe3e2ed4ff90455b63629538b1d37d
winnerezy <eragbonikpeya@gmail.com>
2025-03-27 16:18:30 +0100

    change: change to typescript

--- mamaput-ai-frontend/src/pages/Home.jsx
+++ mamaput-ai-frontend/src/pages/Home.jsx
@@ -1,10 +1,19 @@
+import { authClient } from "../llib/auth";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <button
+        onClick={() =>
+          authClient.signIn.social({
+            provider: "google",
+            callbackURL: "http://localhost:3000/"
+          })
+        }
+      >sign in</button>
+    </div>
+  );
 };
 
 export default Home;
