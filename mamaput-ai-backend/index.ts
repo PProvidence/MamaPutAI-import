9e8beb45f91e42a415c5e6ca7c1fa7e1741dc4ae
winnerezy <eragbonikpeya@gmail.com>
2025-03-29 12:23:10 +0100

    add user router - remove supabase.js - add types to edit user service

--- mamaput-ai-backend/index.ts
+++ mamaput-ai-backend/index.ts
@@ -2,8 +2,9 @@ import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import { configDotenv } from "dotenv";
 import express from "express";
-import { auth } from "./lib/auth.ts";
 import { aiRouter } from "./routers/AIRouter.ts";
+import { userRouter } from "./routers/UserRouter.ts";
+import { auth } from "./lib/auth.ts";
 configDotenv({ path: "./.env" });
 const app = express();
 
@@ -11,7 +12,8 @@ const app = express();
 app.all("/api/auth/*", toNodeHandler(auth));
 
 app.use(express.json());
-app.use(aiRouter)
+app.use("/ai", aiRouter)
+app.use("/user", userRouter)
 app.use(cors({ origin: ["http://localhost:5173"] }));
 
 const port = process.env.PORT || 3005;
