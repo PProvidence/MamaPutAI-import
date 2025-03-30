a74e6df430540e843c9e930d2374e8df1f4b86ea
winnerezy <eragbonikpeya@gmail.com>
2025-03-30 19:26:13 +0100

    add prod url for auth

--- mamaput-ai-frontend/lib/authclient.js
+++ mamaput-ai-frontend/lib/authclient.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { emailOTPClient } from "better-auth/client/plugins"
 
 export const authClient =  createAuthClient({
-    baseURL: "http://localhost:3005",
+    baseURL: "https://api.mamaplaceai.tech",
     plugins: [
         emailOTPClient()
     ]
