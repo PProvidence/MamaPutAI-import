02f505f76a32686495ceee8f6c2d3aeb8ea63d25
winnerezy <eragbonikpeya@gmail.com>
2025-03-31 09:35:45 +0100

    fix wrong router

--- mamaput-ai-backend/routers/FeedbackRouter.js
+++ mamaput-ai-backend/routers/FeedbackRouter.js
@@ -5,5 +5,5 @@ import { getFeedback, storeFeedback } from "../services/Feedback.js";
 
 export const feedbackRouter = express.Router()
  
-aiRouter.post('/store-feedback', storeFeedback);
-aiRouter.get('/get-feedback', getFeedback);
+feedbackRouter.post('/store-feedback', storeFeedback);
+feedbackRouter.get('/get-feedback', getFeedback);
--- mamaput-ai-backend/routers/MealRouter.js
+++ mamaput-ai-backend/routers/MealRouter.js
@@ -6,6 +6,6 @@ import { getNutrients, storeMeal } from "../services/NutrientTracker.js";
 
 export const mealRouter = express.Router()
  
-aiRouter.post('/get-meals', getMeals)
-aiRouter.post('/store-meals', storeMeal)
-aiRouter.get('/get-nutrients/:userId', getNutrients)
+mealRouter.post('/get-meals', getMeals)
+mealRouter.post('/store-meals', storeMeal)
+mealRouter.get('/get-nutrients/:userId', getNutrients)
