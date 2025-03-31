b9bec3b10873cf6fef15a058681b093099ab2497
winnerezy <eragbonikpeya@gmail.com>
2025-03-31 08:06:42 +0100

    add nutrition tracker, feedback

--- mamaput-ai-backend/routers/AIRouter.js
+++ mamaput-ai-backend/routers/AIRouter.js
@@ -1,6 +1,14 @@
 import express from "express";
 import { getMeals } from "../services/AIService.js";
+import {storeFeedback, getFeedback} from "../services/Feedback.js";
+import {getNutrients, storeMeal} from "../services/NutrientTracker.js";
+
+
 
 export const aiRouter = express.Router()
  
 aiRouter.post('/meals', getMeals)
+aiRouter.post('/store-meals', storeMeal)
+aiRouter.get('/get-nutrients/:userId', getNutrients)
+aiRouter.post('/store-feedback', storeFeedback);
+aiRouter.get('/get-feedback', getFeedback);
